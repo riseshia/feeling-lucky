45b69b3f0d7a2f40f2c92fe70ceac220d546cd58
Sangyong Sim <sangyong-sim@cookpad.com>
2020-03-10 20:46:06 +0900

    Handle new line...

--- src/containers/QuotationBox.tsx
+++ src/containers/QuotationBox.tsx
@@ -1,10 +1,14 @@
 /** @jsx jsx */
 
-import { jsx } from "@emotion/core";
+import { jsx, css } from "@emotion/core";
 import { Card, Elevation } from "@blueprintjs/core";
 
+const textStyle = css`
+  white-space: pre;
+`;
+
 export const QuotationBox = (props: { text: string }) => (
-  <Card interactive={false} elevation={Elevation.ZERO}>
+  <Card interactive={false} elevation={Elevation.ZERO} css={textStyle}>
     {props.text}
   </Card>
 );
