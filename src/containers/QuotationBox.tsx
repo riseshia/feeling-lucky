ce5cc84e3aa514322c0431b27bba6edb4e8d012b
Sangyong Sim <sangyong-sim@cookpad.com>
2020-03-13 14:57:36 +0900

    Adjust text visual

--- src/containers/QuotationBox.tsx
+++ src/containers/QuotationBox.tsx
@@ -5,8 +5,9 @@ import { Card, Elevation } from "@blueprintjs/core";
 
 const textStyle = css`
   p {
-    margin: 3px 0 3px 0;
+    margin: 2px 0 2px 0;
     min-height: 10px;
+    line-height: 1.4;
   }
 `;
 
