cecd38cccb7887630a45e1723c7a8dee70267511
Sangyong Sim <sangyong-sim@cookpad.com>
2020-03-13 14:38:20 +0900

    Better random pick

--- src/DataStore.ts
+++ src/DataStore.ts
@@ -1,15 +1,27 @@
 export class DataStore {
   quotations: Array<string>;
+  queue: Array<string>;
   constructor() {
     this.quotations = [];
+    this.queue = [];
   }
 
   update(quotations: Array<string>) {
     this.quotations = quotations;
+    this.shuffle();
   }
 
   pick(): string {
-    return this.quotations[this.pickIdx()];
+    if (this.queue.length === 0) {
+      this.shuffle();
+    }
+    return this.queue.pop()!;
+  }
+
+  private shuffle() {
+    const quotationWithOrder: Array<[string, number]> = this.quotations.map(q => [q, Math.random()]);
+    quotationWithOrder.sort(([_q1, a], [_q2, b]) => a - b);
+    this.queue = quotationWithOrder.map(([q, _order]) => q);
   }
 
   private pickIdx() {
