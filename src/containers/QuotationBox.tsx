92e7d8813ccd180206f000d3a2f50e76c7a3edc9
Sangyong Sim <sangyong-sim@cookpad.com>
2020-03-13 14:41:38 +0900

    Adjust min height for empty line

--- src/containers/QuotationBox.tsx
+++ src/containers/QuotationBox.tsx
@@ -4,7 +4,7 @@ import { jsx, css } from "@emotion/core";
 import { Card, Elevation } from "@blueprintjs/core";
 
 const textStyle = css`
-p { margin: 5px 0 5px 0; }
+p { margin: 5px 0 5px 0; min-height: 10px; }
 `;
 
 export const QuotationBox = (props: { text: string }) => (
